a0bff9c5312e66ad087cbed27032eb70478f0d8e
simple-jie <jiexingbo@gmail.com>
2016-04-20 12:45:39 +0800

    滚轮的大小和位置根据布局大小动态计算, 不受显示内容影响

--- app/src/main/java/com/weidongjian/meitu/wheelviewdemo/MainActivity.java
+++ app/src/main/java/com/weidongjian/meitu/wheelviewdemo/MainActivity.java
@@ -12,23 +12,24 @@ import com.weidongjian.meitu.wheelviewdemo.view.LoopView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private RelativeLayout rootview;
-    private RelativeLayout.LayoutParams layoutParams;
+//    private RelativeLayout rootview;
+//    private RelativeLayout.LayoutParams layoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+//        layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
 
-        rootview = (RelativeLayout) findViewById(R.id.rootview);
+//        rootview = (L) findViewById(R.id.rootview);
 
-        LoopView loopView = new LoopView(this);
+        LoopView loopView = (LoopView) findViewById(R.id.LoopView);
+        LoopView loopView2 = (LoopView) findViewById(R.id.LoopView2);
         ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
-            list.add("item " + i);
+        for (int i = 0; i < 24; i++) {
+            list.add("" + i);
         }
         //设置是否循环播放
         //loopView.setNotLoop();
@@ -41,11 +42,14 @@ public class MainActivity extends AppCompatActivity {
         });
         //设置原始数据
         loopView.setItems(list);
+        loopView2.setItems(list);
         //设置初始位置
-        loopView.setInitPosition(5);
+        loopView.setInitPosition(22);
+        loopView2.setInitPosition(8);
         //设置字体大小
-        loopView.setTextSize(30);
-        rootview.addView(loopView, layoutParams);
+        loopView.setTextSize(24);
+        loopView2.setTextSize(24);
+//        rootview.addView(loopView, layoutParams);
 
     }
 
